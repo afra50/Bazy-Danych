2d8842bae4844803c862c82cf9c8dd805b7c5513
Emilia <afra57@onet.pl>
2024-10-22 19:05:08 +0200

    Sign-up form background & link to Sign in

--- projekt/src/components/pages/Sign_up.js
+++ projekt/src/components/pages/Sign_up.js
@@ -71,105 +71,112 @@ function SignUp() {
   };
 
   return (
-    <div className="sign-form">
-      <h2>Załóż konto</h2>
-      <form onSubmit={handleSubmit}>
-        {/* Imię */}
-        <div className="form-group">
-          <label>Imię</label>
-          <input
-            placeholder="Imię"
-            type="text"
-            id="imie"
-            name="imie"
-            value={formData.imie}
-            onChange={handleChange}
-            required
-          />
-          {errors.imie && <span className="error">{errors.imie}</span>}
-        </div>
+    <div className="sign_container">
+      <div className="sign-form">
+        <h2>Załóż konto</h2>
+        <form onSubmit={handleSubmit}>
+          {/* Imię */}
+          <div className="form-group">
+            <label>Imię</label>
+            <input
+              placeholder="Imię"
+              type="text"
+              id="imie"
+              name="imie"
+              value={formData.imie}
+              onChange={handleChange}
+              required
+            />
+            {errors.imie && <span className="error">{errors.imie}</span>}
+          </div>
 
-        {/* Nazwisko */}
-        <div className="form-group">
-          <label>Nazwisko</label>
-          <input
-            placeholder="Nazwisko"
-            type="text"
-            id="nazwisko"
-            name="nazwisko"
-            value={formData.nazwisko}
-            onChange={handleChange}
-            required
-          />
-          {errors.nazwisko && <span className="error">{errors.nazwisko}</span>}
-        </div>
+          {/* Nazwisko */}
+          <div className="form-group">
+            <label>Nazwisko</label>
+            <input
+              placeholder="Nazwisko"
+              type="text"
+              id="nazwisko"
+              name="nazwisko"
+              value={formData.nazwisko}
+              onChange={handleChange}
+              required
+            />
+            {errors.nazwisko && (
+              <span className="error">{errors.nazwisko}</span>
+            )}
+          </div>
 
-        {/* Telefon */}
-        <div className="form-group">
-          <label>Numer telefonu</label>
-          <input
-            placeholder="Numer telefonu"
-            type="text"
-            id="telefon"
-            name="telefon"
-            value={formData.telefon}
-            onChange={handleChange}
-            required
-          />
-          {errors.telefon && <span className="error">{errors.telefon}</span>}
-        </div>
+          {/* Email */}
+          <div className="form-group">
+            <label>Adres e-mail</label>
+            <input
+              placeholder="Adres e-mail"
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            {errors.email && <span className="error">{errors.email}</span>}
+          </div>
 
-        {/* Email */}
-        <div className="form-group">
-          <label>Adres e-mail</label>
-          <input
-            placeholder="Adres e-mail"
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          {errors.email && <span className="error">{errors.email}</span>}
-        </div>
+          {/* Telefon */}
+          <div className="form-group">
+            <label>Numer telefonu</label>
+            <input
+              placeholder="Numer telefonu"
+              type="text"
+              id="telefon"
+              name="telefon"
+              value={formData.telefon}
+              onChange={handleChange}
+              required
+            />
+            {errors.telefon && <span className="error">{errors.telefon}</span>}
+          </div>
 
-        {/* Hasło */}
-        <div className="form-group">
-          <label>Hasło</label>
-          <input
-            placeholder="Hasło"
-            type="password"
-            id="haslo"
-            name="haslo"
-            value={formData.haslo}
-            onChange={handleChange}
-            required
-          />
-          {errors.haslo && <span className="error">{errors.haslo}</span>}
-        </div>
+          {/* Hasło */}
+          <div className="form-group">
+            <label>Hasło</label>
+            <input
+              placeholder="Hasło"
+              type="password"
+              id="haslo"
+              name="haslo"
+              value={formData.haslo}
+              onChange={handleChange}
+              required
+            />
+            {errors.haslo && <span className="error">{errors.haslo}</span>}
+          </div>
 
-        {/* Potwierdzenie hasła */}
-        <div className="form-group">
-          <label>Potwierdź hasło</label>
-          <input
-            placeholder="Potwierdź hasło"
-            type="password"
-            id="potwierdzhaslo"
-            name="potwierdzhaslo"
-            value={formData.potwierdzhaslo}
-            onChange={handleChange}
-            required
-          />
-          {errors.potwierdzhaslo && (
-            <span className="error">{errors.potwierdzhaslo}</span>
-          )}
-        </div>
-
-        <button type="submit" className="btn-submit">
-          Zarejestruj
-        </button>
-      </form>
+          {/* Potwierdzenie hasła */}
+          <div className="form-group">
+            <label>Potwierdź hasło</label>
+            <input
+              placeholder="Potwierdź hasło"
+              type="password"
+              id="potwierdzhaslo"
+              name="potwierdzhaslo"
+              value={formData.potwierdzhaslo}
+              onChange={handleChange}
+              required
+            />
+            {errors.potwierdzhaslo && (
+              <span className="error">{errors.potwierdzhaslo}</span>
+            )}
+          </div>
+          <div className="to_sign_in">
+            <span>Masz już konto?</span>
+            <a href="/SignIn">Zaloguj się</a>
+          </div>
+          <button type="submit" className="btn-submit">
+            Zarejestruj
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
