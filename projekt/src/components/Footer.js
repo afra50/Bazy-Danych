39fcb74772ff73b66058d94043bf87d2d54ca872
Emilia <afra57@onet.pl>
2024-10-23 15:40:57 +0200

    footer basic styles

--- projekt/src/components/Footer.js
+++ projekt/src/components/Footer.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "../styles/Footer.scss";
+import logo from "../assets/images/logo.png";
 
 function Footer() {
   return (
-    <footer>
-      <p>© 2024 Moja Strona. Wszelkie prawa zastrzeżone.</p>
+    <footer className="footer">
+      <div className="column_footer">
+        <img src={logo} alt="logo" id="footer_logo"></img>
+      </div>
+      <div className="column_footer">
+        <h2>Kontakt</h2>
+        <div className="Kontakt">
+          +48 728 273 283
+          <br></br>
+          wyjedzstad@mail.com
+        </div>
+      </div>
+      <div className="column_footer">
+        <h2>Konto klienta</h2>
+        <a href="/SignIn">Zaloguj się</a>
+        <a href="/SignUp">Załóż konto</a>
+        <a href="">Moje rezerwacje</a>
+      </div>
+      <div className="column_footer">
+        <h2>Konto właściciela</h2>
+        <a href="/SignIn">Zaloguj się</a>
+        <a href="">Moje oferty</a>
+        <a href="">Rezerwacje</a>
+      </div>
     </footer>
   );
 }
