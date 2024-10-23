71905db79e17b1eee03568c1f2ab4d84a258d001
Emilia <afra57@onet.pl>
2024-10-23 12:57:41 +0200

    Szyfrowanie hasla

--- projekt/backend/server.js
+++ projekt/backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 5000;
@@ -27,22 +28,36 @@ db.connect((err) => {
 });
 
 // Endpoint do obsługi rejestracji
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { imie, nazwisko, telefon, email, haslo } = req.body;
 
+  // Walidacja pól
   if (!imie || !nazwisko || !telefon || !email || !haslo) {
     return res.status(400).send("Wszystkie pola są wymagane");
   }
 
-  const sql = `INSERT INTO klienci (imie, nazwisko, telefon, email, haslo, data_rejestracji) VALUES (?, ?, ?, ?, ?, NOW())`;
-  db.query(sql, [imie, nazwisko, telefon, email, haslo], (err, result) => {
-    if (err) {
-      console.error("Błąd podczas wstawiania danych:", err);
-      res.status(500).send("Błąd serwera");
-    } else {
-      res.status(201).send("Rejestracja zakończona sukcesem");
-    }
-  });
+  try {
+    // Szyfrowanie hasła
+    const hashedPassword = await bcrypt.hash(haslo, 10); // 10 to liczba rund haszowania
+
+    // Zapytanie SQL do dodania użytkownika
+    const sql = `INSERT INTO klienci (imie, nazwisko, telefon, email, haslo, data_rejestracji) VALUES (?, ?, ?, ?, ?, NOW())`;
+    db.query(
+      sql,
+      [imie, nazwisko, telefon, email, hashedPassword],
+      (err, result) => {
+        if (err) {
+          console.error("Błąd podczas wstawiania danych:", err);
+          return res.status(500).send("Błąd serwera");
+        }
+
+        res.status(201).send("Rejestracja zakończona sukcesem");
+      }
+    );
+  } catch (error) {
+    console.error("Błąd podczas szyfrowania hasła:", error);
+    res.status(500).send("Błąd serwera podczas rejestracji");
+  }
 });
 
 app.listen(port, () => {
