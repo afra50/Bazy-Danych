ab42bd4197d84933f3f3e8006c77e4f07764fa09
Julia <twoj_email@example.com>
2024-10-25 00:06:03 +0200

    Footer, odstępy w nagłówkach, animacja linków

--- projekt/src/components/Footer.js
+++ projekt/src/components/Footer.js
@@ -5,7 +5,8 @@ import logo from "../assets/images/logo.png";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="column_footer">
+      <div className="column_footer kontakt ">
+        
         <img src={logo} alt="logo" id="footer_logo"></img>
       </div>
       <div className="column_footer">
@@ -14,15 +15,21 @@ function Footer() {
           +48 728 273 283
           <br></br>
           wyjedzstad@mail.com
+          <br></br>
+          pn-pt   8:00-22:00
+          <br></br>
+          sb-nd   9:00-20:00	
+
+
         </div>
       </div>
-      <div className="column_footer">
+      <div className="column_footer konto_klienta">
         <h2>Konto klienta</h2>
         <a href="/SignIn">Zaloguj się</a>
         <a href="/SignUp">Załóż konto</a>
         <a href="">Moje rezerwacje</a>
       </div>
-      <div className="column_footer">
+      <div className="column_footer konto_wlasciciela">
         <h2>Konto właściciela</h2>
         <a href="/SignInAsOwner">Zaloguj się</a>
         <a href="">Moje oferty</a>
