58eb26cfb3b9f606eb33a6f462028dda901a9017
Emilia <afra57@onet.pl>
2024-10-25 17:56:26 +0200

    footer update

--- projekt/src/components/Footer.js
+++ projekt/src/components/Footer.js
@@ -5,31 +5,22 @@ import logo from "../assets/images/logo.png";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="column_footer kontakt ">
-        
+      <div className="column_footer">
         <img src={logo} alt="logo" id="footer_logo"></img>
+        <p>
+          <span>wyjedzstad@mail.com</span>
+          <span>+48 728 273 283</span>
+          <span>pn-pt 8:00-22:00</span>
+          <span>sb-nd 9:00-20:00</span>
+        </p>
       </div>
       <div className="column_footer">
-        <h2>Kontakt</h2>
-        <div className="Kontakt">
-          +48 728 273 283
-          <br></br>
-          wyjedzstad@mail.com
-          <br></br>
-          pn-pt   8:00-22:00
-          <br></br>
-          sb-nd   9:00-20:00	
-
-
-        </div>
-      </div>
-      <div className="column_footer konto_klienta">
         <h2>Konto klienta</h2>
         <a href="/SignIn">Zaloguj się</a>
         <a href="/SignUp">Załóż konto</a>
         <a href="">Moje rezerwacje</a>
       </div>
-      <div className="column_footer konto_wlasciciela">
+      <div className="column_footer">
         <h2>Konto właściciela</h2>
         <a href="/SignInAsOwner">Zaloguj się</a>
         <a href="">Moje oferty</a>
