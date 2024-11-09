d336c6947fed0880d3dcdf57a4d783b3959862b1
Julia <twoj_email@example.com>
2024-11-09 13:02:59 +0100

    recomended scale

--- projekt/backend/models/db.js
+++ projekt/backend/models/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "127.0.0.1",
+  host: process.env.DB_HOST || "95.215.232.175",
   port: process.env.DB_PORT || "3306",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "puqu-gute-luna",
   database: process.env.DB_NAME || "strona_z_rezerwacjami",
 });
 
