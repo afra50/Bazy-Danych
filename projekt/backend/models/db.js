3641c8db781adc37ae5771425dd3fed0f7c8795f
Emilia <afra57@onet.pl>
2025-01-04 01:32:15 +0100

    ip update

--- projekt/backend/models/db.js
+++ projekt/backend/models/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "127.0.0.1",
+  host: process.env.DB_HOST || "192.168.0.3",
   port: process.env.DB_PORT || "3306",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
